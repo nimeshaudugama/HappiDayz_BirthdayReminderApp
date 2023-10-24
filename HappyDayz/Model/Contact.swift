8aa1af53214812039318f13cc3da816a65c52606
Abilash <a_raveendran195815@fanshaweonline.ca>
2023-10-24 19:40:24 -0400

    [#1]Implements easy option to add, update and delete birthdays.

--- HappyDayz/Model/Contact.swift
+++ HappyDayz/Model/Contact.swift
@@ -1,29 +1,4 @@
-//
-//  File.swift
-//  HappyDayz
-//
-//  Created by Nimesha Jayathissa on 2023-10-24.
-//
-
-
 import Foundation
 
-struct Contact: Identifiable {
-    let id = UUID()
-    let firstName: String
-    let lastName: String
-   
-   
-   
-    
-    
-    
-    internal init(firstName: String, lastName: String) {
-        
-        self.firstName = firstName
-        self.lastName = lastName
-        
-        
-      
-    }
-}
+// Staring code for As a user, I want to have a easy option to add, update and delete birthdays.
+
